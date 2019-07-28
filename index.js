ee2040bc320ab4ce0db729b15746f3191338f541
the-best-developer <im.dylan.gosnell@gmail.com>
2019-07-28 18:56:40 -0400

    added cors function

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const PostRoute = require('./routes/posts');
 const CommentRoute = require('./routes/comments');
 const server = express();
+const cors = require('cors');
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Only allow 127.0.0.1:4000 Origin
+        const isAllowed = (origin === "127.0.0.1:4000");
+        callback(isAllowed ? null : "This origin is not allowed", isAllowed);
+    }
+};
 
 server.use(express.json());
+server.use(cors(corsOptions));
 
 server.use('/api/posts', PostRoute);
 server.use('/api/posts', CommentRoute);
-server.use('/', (req, res) => res.send('Server up and running :)'));
+server.use('/', (req, res) => { 
+    res.send('Server up and rudnning :)')
+});
 
 server.listen(4000, () => {
     console.log('Server on 4000');
